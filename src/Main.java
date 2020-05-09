14947ab148de71a0b060a510783b12adc3f10e23
Ergün Akıllı <02484459@ksnet.local>
2020-05-09 23:08:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public static void mesajVer(){
+        System.out.println("Merhaba");
+    }
+
+    public static void hesapla(){
+        System.out.println("Hesaplandı");
+    }
+
+
+}
---
+++ src/degiskenler.java
@@ -0,0 +1,15 @@
+class Degiskenler {
+    public static void main(String[] args) {
+
+        //camelCase
+        //PascalCase
+        int not = 55;
+        int kredi = 100000;
+        double notOrtalamasi= 50.5;
+        String isim ="Engin Demiroğ";
+        boolean sistemeGirisYapmismi = true;
+
+        System.out.println(not);
+        //integer
+    }
+}
